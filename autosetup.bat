81266cb340744f007232810e259d55482ee709c8
KJPOLWIN <85358420+KJPOLWIN@users.noreply.github.com>
2022-05-29 14:20:59 +0200

    Remove MinGW download from installer

--- autosetup.bat
+++ autosetup.bat
@@ -4,19 +4,6 @@ REM Downloading SFML
 PowerShell -command "Invoke-WebRequest https://www.sfml-dev.org/files/SFML-2.5.1-windows-gcc-7.3.0-mingw-32-bit.zip -OutFile '.\SFML.zip'"
 PowerShell -command "Expand-Archive -Force .\SFML.zip .\   "
 
-REM Checking if MinGW is installed, and installing it
-for /f %%I in ('g++ --version') DO set mingw=%%I
-
-if "%mingw%"=="" (
-  echo Installing MinGW. Follow instructions in MinGW installer.
-  PowerShell -command "Invoke-WebRequest https://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win32/Personal%20Builds/mingw-builds/installer/mingw-w64-install.exe -OutFile 'mingw.exe'"
-  call mingw.exe
-  del mingw.exe
-	
-) else (
-  echo MinGW already installed
-)
-
 REM Checking if CMake is installed, and installing it
 for /f %%I in ('cmake --version') DO set cmake=%%I
 
