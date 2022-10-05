488d0440f2385542a658c49b0a487ff63379c4d8
KJPOLWIN <kjpolwin@gmail.com>
2022-10-05 21:52:55 +0200

    Building display (untested)

---
+++ src/building.cpp
@@ -0,0 +1,79 @@
+#include "building.h"
+#include "mapnode.h"
+#include <SFML/Graphics.hpp>
+#include <string>
+
+Building::Building(int x, int y, std::string name, int faction,
+                   int turnsToBuild, 
+                   int upkeep, int population,
+                   int moneyProduction, int foodProduction,
+                   int woodProduction, int stoneProduction,
+                   int weaponsProduction)
+  : positioningNode{ x, y },
+    turnsToBuild{ turnsToBuild },
+    name{ name },
+    faction{ faction },
+    upkeep{ upkeep },
+    population{ population },
+    moneyProduction{ moneyProduction },
+    foodProduction{ foodProduction },
+    woodProduction{ woodProduction },
+    stoneProduction{ stoneProduction },
+    weaponsProduction{ weaponsProduction }
+{
+
+}
+
+void Building::update()
+{
+  if(!completed)
+  {
+    --turnsToBuild;
+    if(turnsToBuild == 0) completed = true;
+  }
+}
+
+sf::Vector2f Building::getPosition()
+{
+  return positioningNode.getPosition();
+}
+      
+void Building::setPosition(HexVector newPosition)
+{
+  positioningNode.setPosition(newPosition);
+}
+
+int Building::getUpkeep()
+{
+  return upkeep;
+}
+
+int Building::getFoodCost()
+{
+  return population;
+}
+
+int Building::getMoneyProduction()
+{
+  return moneyProduction;
+}
+
+int Building::getFoodProduction()
+{
+  return woodProduction;
+}
+
+int Building::getWoodProduction()
+{
+  return woodProduction;
+}
+
+int Building::getStoneProduction()
+{
+  return stoneProduction;
+}
+
+int Building::getWeaponsProduction()
+{
+  return weaponsProduction;
+}
---
+++ src/building.h
@@ -0,0 +1,53 @@
+#ifndef BUILDING_H
+#define BUILDING_H
+
+  #include "mapnode.h"
+  #include <SFML/Graphics.hpp>
+  #include <string>
+
+  class Building
+  {
+    public:
+      Building(int x, int y, std::string name, int faction,
+               int turnsToBuild, 
+               int upkeep, int population,
+               int moneyProduction, int foodProduction,
+               int woodProduction, int stoneProduction,
+               int weaponsProduction);
+      Building() = default;
+
+      void update();
+
+      sf::Vector2f getPosition();
+      void setPosition(HexVector newPosition);
+
+      int getUpkeep();
+      int getFoodCost();
+
+      int getMoneyProduction();
+      int getFoodProduction();
+      int getWoodProduction();
+      int getStoneProduction();
+      int getWeaponsProduction();
+
+    private:
+      MapNode positioningNode{ 0, 0 };
+
+      bool completed{ false };
+      int turnsToBuild{ 3 };
+
+      std::string name{ "" };
+
+      int faction{ 0 };
+
+      int upkeep{ 0 };
+      int population{ 0 };
+
+      int moneyProduction{ 0 };
+      int foodProduction{ 0 };
+      int woodProduction{ 0 };
+      int stoneProduction{ 0 };
+      int weaponsProduction{ 0 };
+  };
+
+#endif
